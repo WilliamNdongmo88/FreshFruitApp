f6650a64d2c80484a35e620224265aaeb32cc633
LFGaius <liburialgaius@gmail.com>
2023-06-08 16:32:24 +0200

    update_code_and_design

--- lib/views/MA_LoadingPage.dart
+++ lib/views/MA_LoadingPage.dart
@@ -16,58 +16,58 @@ class LoadingView extends StatefulWidget {
 
 class _LoadingViewState extends State<LoadingView>
     with TickerProviderStateMixin {
-  GlobalKey<FormState> formKey = GlobalKey<FormState>();
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         body: SingleChildScrollView(
             child: Container(
-                padding: EdgeInsets.only(left: 30, right: 30, top: 20),
-                child: Form(
-                    key: formKey,
-                    child: Column(children: [
-                      //  this image is the logo that we want to display
-                      //  comment test
-                      Image.asset('assets/logo.png'),
-                      SizedBox(
-                        height: Get.height * 0.03,
-                      ),
-                      // first text of our loading page
-                      myText(
-                          text: 'Bienvenue',
-                          style: GoogleFonts.dmSans(
-                              textStyle: TextStyle(
-                                  fontSize: 50, fontWeight: FontWeight.bold)),
-                          textAlign: TextAlign.center),
-                      SizedBox(
-                        height: Get.height * 0.10,
-                      ),
-                      myText(
-                          text: "AppMoney est une application de",
-                          style: GoogleFonts.dmSans(
-                              textStyle: TextStyle(
-                                  fontSize: 15, fontWeight: FontWeight.bold)),
-                          textAlign: TextAlign.center),
-                      myText(
-                          text: "transfert d'argent d'un pays à un autre",
-                          style: GoogleFonts.dmSans(
-                              textStyle: TextStyle(
-                                  fontSize: 15, fontWeight: FontWeight.bold)),
-                          textAlign: TextAlign.center),
-                      SizedBox(
-                        height: Get.height * 0.04,
-                      ),
-                      SizedBox(
-                        height: Get.height * 0.01,
-                      ),
-                      SpinKitCircle(
-                        color: Colors.orange[700],
-                        size: 70.0,
-                        controller: AnimationController(
-                            vsync: this,
-                            duration: const Duration(milliseconds: 1200)),
-                      )
-                    ])))));
+                padding: EdgeInsets.only(left: 30, right: 30, top: 50),
+                child: Column(children: [
+                  //  logo
+                  //  comment test
+                  Image.asset('assets/logo.png'),
+                  SizedBox(
+                    height: Get.height * 0.03,
+                  ),
+                  // first text of our loading page
+                  myText(
+                      text: 'Bienvenue',
+                      style: GoogleFonts.dmSans(
+                          textStyle: TextStyle(
+                              fontSize: 65, fontWeight: FontWeight.bold)),
+                      textAlign: TextAlign.center),
+                  SizedBox(
+                    height: Get.height * 0.10,
+                  ),
+                  myText(
+                      text: "AppMoney est une application de transfert",
+                      style: GoogleFonts.dmSans(
+                          textStyle: TextStyle(
+                              fontSize: 15, fontWeight: FontWeight.bold)),
+                      textAlign: TextAlign.center),
+                  myText(
+                      text: " d'argent d'un pays à un autre",
+                      style: GoogleFonts.dmSans(
+                          textStyle: TextStyle(
+                              fontSize: 15, fontWeight: FontWeight.bold)),
+                      textAlign: TextAlign.center),
+                  SizedBox(
+                    height: Get.height * 0.04,
+                  ),
+                  SizedBox(
+                    height: Get.height * 0.10,
+                  ),
+
+                  // Make a spinner rotation // Make a spinner rotation
+                  // and put the spinner like a button
+
+                  SpinKitFadingCircle(
+                    color: Color.fromARGB(240, 248, 61, 4),
+                    size: 126.0,
+                    controller: AnimationController(
+                        vsync: this,
+                        duration: const Duration(milliseconds: 120)),
+                  )
+                ]))));
   }
 }
