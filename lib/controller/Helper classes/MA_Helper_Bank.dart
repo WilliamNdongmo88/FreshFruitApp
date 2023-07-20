928d0f89fc34888904ace13ac2097791ce86ae9d
unknown <evariste>
2023-07-20 10:43:27 +0100

    last commit on dev

--- lib/controller/Helper classes/MA_Helper_Bank.dart
+++ lib/controller/Helper classes/MA_Helper_Bank.dart
@@ -2,7 +2,8 @@
 class MA_Helper_Bank{
   String?  Name;
   String? Intitule;
+  String? numCompte;
 
-  MA_Helper_Bank(this.Name,this.Intitule);
+  MA_Helper_Bank(this.Name,this.Intitule,this.numCompte);
 
 }
